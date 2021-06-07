bd228cd91e7ae411896a03ae21e4888fd29945db
nelgoez <76503414+nelgoez@users.noreply.github.com>
2021-06-07 20:17:19 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Nav from './components/Nav';
 export default function App() {
 
   const [cities, setCities] = useState([]);
-  const { APIKEY } = process.env;
+  const APIKEY = '4ae2636d8dfbdc3044bede63951a019b';
 
   function onSearch(pueblito) {
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${pueblito}&appid=${APIKEY}&units=metric`)
